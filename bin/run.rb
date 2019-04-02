fafceea400cf32deeb78572af5d6c58ceea28a54
github username <github email address>
2019-04-02 16:50:16 +0100

    Done

--- bin/run.rb
+++ bin/run.rb
@@ -2,7 +2,7 @@
 
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
-
+require 'pry'
 welcome
 character = get_character_from_user
 show_character_movies(character)
--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,9 +3,22 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character_name)
-  #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
+  url = "http://swapi.co/api/people/?search=#{character_name}"
+
+  response_string = RestClient.get(url)
   response_hash = JSON.parse(response_string)
+
+  film = response_hash["results"][0]["films"]
+  film_array = []
+  film.each do |title|
+    film_url = RestClient.get(title)
+    film_hash = JSON.parse(film_url)
+    film_array << film_hash["title"]
+  end
+  return film_array
+
+end
+
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
@@ -16,9 +29,9 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
-end
 
 def print_movies(films)
+  puts films
   # some iteration magic and puts out the movies in a nice list
 end
 
@@ -26,7 +39,6 @@ def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_movies(films)
 end
-
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
